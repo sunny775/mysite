48d7c901bc1409db4b0f65de663b783ee32d2521
Sunny Cletus <scletus@gmail.com>
2020-06-20 13:02:10 +0100

    changed logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import me from './images/me.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={me} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nkwuda Sunday Cletus
+        </p>
+        <p>
+          A web and mobile app developer
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://twitter.com/scletus40"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        twitter
         </a>
       </header>
     </div>
